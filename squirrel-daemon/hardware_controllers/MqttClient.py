732ce0ba5c2632ecfe16204d9d5919d3340d3e37
donutsoft <paul@murphymail.eu>
2025-09-06 23:31:27 -0700

    Implement water controller

---
+++ squirrel-daemon/hardware_controllers/MqttClient.py
@@ -0,0 +1,84 @@
+from collections import defaultdict
+import paho.mqtt.client as mqtt
+import logging
+import time
+
+class MqttClient:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self._connected = False
+        self.topics = {}
+        self._mqttClient = mqtt.Client()
+        self._mqttClient.username_pw_set('mqtt', 'mqtt')
+        self._mqttClient.on_connect = self.onConnect
+        self._mqttClient.on_message = self.onMessage
+        self._mqttClient.on_disconnect = self.onDisconnect
+        
+        # Try to connect, but don't block if it fails
+        try:
+            self._mqttClient.connect('192.168.1.3', 1883)
+            self._mqttClient.loop_start()
+        except Exception as e:
+            self.logger.error(f"Failed to connect to MQTT server: {e}")
+            self._connected = False
+
+    def onConnect(self, client, userdata, flags, rc):
+        if rc == 0:
+            self._connected = True
+            print("Connected to MQTT server")
+            for topic in self.topics:
+                print("Subscribing to " + topic)
+                self._mqttClient.subscribe(topic)
+        else:
+            self._connected = False
+            self.logger.error(f"Failed to connect to MQTT server with code: {rc}")
+
+    def onDisconnect(self, client, userdata, rc):
+        self._connected = False
+        if rc != 0:
+            self.logger.warning(f"Unexpected disconnection from MQTT server (code: {rc})")
+            # Try to reconnect
+            try:
+                self._mqttClient.reconnect()
+            except Exception as e:
+                self.logger.error(f"Failed to reconnect to MQTT server: {e}")
+
+    def onMessage(self, client, userdata, msg):
+        try:
+            if msg.topic in self.topics:
+                for callback in self.topics[msg.topic]:
+                    callback(msg.payload.decode('utf-8'))
+        except Exception as e:
+            self.logger.exception("Unhandled exception")
+            raise
+    
+    def subscribe(self, topic, callback):
+        shouldSubscribe = False
+        if not topic in self.topics:
+            self.topics[topic] = []
+            self.topics[topic].append(callback)
+            shouldSubscribe = True
+        
+        if self._connected and shouldSubscribe:
+            self.logger.info("Post Subscribing to topic %s", topic)
+            self._mqttClient.subscribe(topic)
+
+    def unsubscribe(self, topic, callback):
+        if topic in self.topics:
+            if callback in self.topics[topic]:
+                self.topics[topic].remove(callback)
+                if len(self.topics[topic]) == 0:
+                    del self.topics[topic]
+                    self._mqttClient.unsubscribe(topic)
+                    self.logger.info("Unsubscribed from topic %s", topic)
+            else:
+                self.logger.warning("Callback not found in topic %s", topic)
+        else:
+            self.logger.warning("Topic %s not found", topic)
+
+    def publish(self, topic, message):
+        if not self._connected:
+            print(f"Cannot publish message - MQTT not connected")
+            return
+        print("Sending message %s to topic %s", message, topic)
+        self._mqttClient.publish(topic, message)
